cd4d64dc9655f7eb5fb0bb3e7e2bf8693d93789b
Koltai Kadosa <kadosa.koltai@gmail.com>
2021-03-07 20:40:27 +0100

    end

--- Compact/Compact/Compact.cpp
+++ Compact/Compact/Compact.cpp
@@ -20,14 +20,18 @@ int main()
 	int rank = MPI::COMM_WORLD.Get_rank();
 	int cluster = MPI::COMM_WORLD.Get_size();
 	const unsigned int size = 1000;
-	const float vmax = 100;
+	const float vmax = 10;
 
 
-	std::vector<float> values;
-	std::vector<int> column;
-	std::vector<int> row;
-	std::vector<float> vector(size);
-	int sizes[4];
+	std::vector<float> valuesV;
+	std::vector<int> columnV;
+	std::vector<int> rowV;
+	float vector[size];
+
+	int sizes[3];
+	float* values;
+	int* column;
+	int* row;
 
 	if (rank == 0) {
 		std::random_device rd;
@@ -48,68 +52,79 @@ int main()
 				vector[j] = val(gen);
 		}
 		for (int i = 0; i < size; ++i) {
-			row.push_back(values.size());
+			rowV.push_back(valuesV.size());
 			for (int j = 0; j < size; ++j) {
 				float var = temp[i][j];
 				if (var != 0) {
-					values.push_back(var);
-					column.push_back(i);
+					valuesV.push_back(var);
+					columnV.push_back(i);
 				}
 			}
 		}
-		sizes[0] = values.size();
-		sizes[1] = column.size();
-		sizes[2] = row.size();
-		sizes[3] = vector.size();
-	}
-	MPI::COMM_WORLD.Bcast(sizes, 4, MPI_INT, 0);
-	MPI::COMM_WORLD.Bcast(values.data(), sizes[0], MPI_FLOAT, 0);
-	MPI::COMM_WORLD.Bcast(column.data(), sizes[1], MPI_FLOAT, 0);
-	MPI::COMM_WORLD.Bcast(row.data(), sizes[2], MPI_FLOAT, 0);
-	MPI::COMM_WORLD.Bcast(vector.data(), sizes[3], MPI_FLOAT, 0);
+		rowV.push_back(size);
 
-	std::cout << values.size() << std::endl;
-	std::cout << column.size() << std::endl;
-	std::cout << row.size() << std::endl;
-	std::cout << vector.size() << std::endl;
-	/*
+		sizes[0] = valuesV.size();
+		sizes[1] = columnV.size();
+		sizes[2] = rowV.size();
+		values = valuesV.data();
+		column = columnV.data();
+		row = rowV.data();
+	}
+
+	//BEGIN
 	auto start = std::chrono::high_resolution_clock::now();
-	std::vector<int> work(chunk);
-	MPI::COMM_WORLD.Scatter(vector.data(), chunk, MPI_INT32_T, work.data(), chunk, MPI_INT32_T, 0);
-	std::vector<int> out(chunk);
-	for (int i = 0; i < chunk; ++i) {
-		if (work[i] < barrier) {
-			out[i] = 1;
-		}
-		else {
-			out[i] = 0;
-		}
-	}
-	std::vector<int> middle(size + extra);
-	MPI::COMM_WORLD.Gather(out.data(), chunk, MPI_INT32_T, middle.data(), chunk, MPI_INT32_T, 0);
-	MPI::Op* op = new MPI::Op();
-	op->Init((MPI::User_function*)add, true);
-	std::vector<int> last(size + extra);
 
-	MPI::COMM_WORLD.Exscan(middle.data(), last.data(), middle.size(), MPI_INT32_T, *op);
-	op->Free();
+	MPI::COMM_WORLD.Bcast(sizes, 3, MPI_INT, 0);
+	if (rank != 0) {
+		values = new float[sizes[0]];
+		column = new int[sizes[1]];
+		row = new int[sizes[2]];
+	}
+	MPI::COMM_WORLD.Bcast(values, sizes[0], MPI_FLOAT, 0);
+	MPI::COMM_WORLD.Bcast(column, sizes[1], MPI_INT, 0);
+	MPI::COMM_WORLD.Bcast(row, sizes[2], MPI_INT, 0);
+	MPI::COMM_WORLD.Bcast(vector, size, MPI_FLOAT, 0);
+
+	float result[size] = {};
+	for (int i = rank; i < size; i += cluster) {
+		float accumulator = 0;
+		int begin = row[i];
+		int end = row[i + 1];
+		for (int j = begin; j < end; ++j) {
+			int left = values[j];
+			int right = vector[column[j]];
+			accumulator += left * right;
+		}
+		result[i] = accumulator;
+	}
+	
+	float* receive;
+	if (rank == 0) {
+		receive = new float[size*cluster];
+	}
+
+	MPI::COMM_WORLD.Gather(result, size, MPI_FLOAT, receive, size, MPI_FLOAT, 0);
 
 	if (rank == 0) {
-		std::cout << "m" << std::endl;
-		int result = last[last.size() - 1] + 1;
-		std::cout << "l" << std::endl;
-		std::vector<int> compacted(result);
-		for (int i = 0; i < last.size(); ++i) {
-			std::cout << i << std::endl;
-			if (middle[i]) {
-				compacted[last[i]] = vector[i];
+		float out[size];
+		for (int i = 0; i < size; i += cluster) {
+			for (int j = 0; j < cluster && i + j < size; ++j) {
+				int index = j * size + i;
+				out[i + j] = receive[index];
 			}
 		}
-
+		//END
 		auto end = std::chrono::high_resolution_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-		std::cout << duration.count() << " milliseconds" << std::endl;
+		std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
 	}
-	MPI::Finalize();*/
+
+	if (rank != 0) {
+		delete[] values;
+		delete[] column;
+		delete[] row;
+	} else {
+		delete[] receive;
+	}
+	MPI::Finalize();
 	return 0;
 }
