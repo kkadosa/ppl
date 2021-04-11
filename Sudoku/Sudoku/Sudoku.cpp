287773fc6fa400396ef3749db9c9dfd1070721ff
Koltai Kadosa <kadosa.koltai@gmail.com>
2021-04-11 23:06:19 +0200

    6

--- Sudoku/Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku/Sudoku.cpp
@@ -54,8 +54,10 @@ bool isSolved(std::vector<int> board) {
 
 void solveBack(std::vector<int> board) {
 	if (isSolved(board)) {
+		std::cout << "FF" << std::endl;
 		MPI::COMM_WORLD.Send(board.data(), size * size, MPI_INT, 0, 0);
 	} else {
+		std::cout << "GG" << std::endl;
 		for (int i = 0; i < size; ++i) {
 			for (int j = 0; j < size; ++j) {
 				for (int k = 0; k < size; ++k) {
