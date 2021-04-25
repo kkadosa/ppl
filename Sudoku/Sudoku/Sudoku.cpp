4eca1b8b2fa864f2f4f05c8a71c6d43dca67f21d
Koltai Kadosa <kadosa.koltai@gmail.com>
2021-04-26 01:59:14 +0200

    FUC

--- Sudoku/Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku/Sudoku.cpp
@@ -11,13 +11,8 @@ const int size = 9;
 
 bool isAllowed(const std::vector<int>& board, int x, int y, int digit) {
 	bool allowed = true;
-	/*
-		for (int i = 0; i < size * size; ++i) {
-			std::cout << board[i];
-		}
-		std::cout << std::endl;
-		*/
-	if (board[y * size + x] != 0) {
+	
+	if (board[y * size + x] != 0) { 
 		allowed = false;
 	}
 
@@ -63,9 +58,15 @@ void solveBack(const std::vector<int>& board) {
 			for (int x = 0; x < size; ++x) {
 				for (int k = 1; k <= size; ++k) {
 					if (isAllowed(board, x, y, k)) {
+
+						for (int i = 0; i < size * size; ++i) {
+							std::cout << board[i];
+						}
+						std::cout << std::endl;
+
 						//std::cout << k << "?" << std::endl;
 						std::vector<int> t(board);
-						t[y * size + k] = k;
+						t[y * size + x] = k;
 						//std::cout << k << "!" << std::endl;
 						solveBack(t);
 					}
