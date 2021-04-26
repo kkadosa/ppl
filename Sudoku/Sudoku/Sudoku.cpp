c73f3cd869b70c0d08fab3029eec713f36abe4a1
Koltai Kadosa <kadosa.koltai@gmail.com>
2021-04-26 02:15:08 +0200

    FUCK C

--- Sudoku/Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku/Sudoku.cpp
@@ -49,6 +49,12 @@ bool isSolved(const std::vector<int>& board) {
 }
 
 void solveBack(const std::vector<int>& board) {
+
+	for (int i = 0; i < size * size; ++i) {
+		std::cout << board[i];
+	}
+	std::cout << std::endl;
+
 	if (isSolved(board)) {
 		MPI::COMM_WORLD.Send(board.data(), size * size, MPI_INT, 0, 0);
 	} else {
@@ -65,12 +71,6 @@ void solveBack(const std::vector<int>& board) {
 				if (possibilities.empty()) {
 					go = false;
 				} else {
-
-					for (int i = 0; i < size * size; ++i) {
-						std::cout << board[i];
-					}
-					std::cout << std::endl;
-
 					for (int k : possibilities) {
 						std::vector<int> t(board);
 						t[y * size + x] = k;
